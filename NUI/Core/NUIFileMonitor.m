920416e64fbc504d8876290ddfef23eeeea8d6ca
Tom Benner <tombenner@gmail.com>
2013-01-14 00:01:19 -0800

    Cleaned up NUIFileMonitor, removed unnecessary source types

--- NUI/Core/NUIFileMonitor.m
+++ NUI/Core/NUIFileMonitor.m
@@ -13,15 +13,15 @@
 +(void)watch:(NSString*)path withCallback:(void(^)())callback
 {
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-	int fildes = open([path UTF8String], O_EVTONLY);
+	int fileDescriptor = open([path UTF8String], O_EVTONLY);
     
-    __block dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,fildes,
-															  DISPATCH_VNODE_DELETE | DISPATCH_VNODE_WRITE | DISPATCH_VNODE_EXTEND | DISPATCH_VNODE_ATTRIB | DISPATCH_VNODE_LINK | DISPATCH_VNODE_RENAME | DISPATCH_VNODE_REVOKE,
-															  queue);
+    __block dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,fileDescriptor,
+                                                              DISPATCH_VNODE_DELETE | DISPATCH_VNODE_WRITE | DISPATCH_VNODE_EXTEND,
+                                                              queue);
 	dispatch_source_set_event_handler(source, ^
                                       {
                                           unsigned long flags = dispatch_source_get_data(source);
-                                          if(flags & DISPATCH_VNODE_DELETE) {
+                                          if (flags & DISPATCH_VNODE_DELETE) {
                                               dispatch_source_cancel(source);
                                               callback();
                                               [self watch:path withCallback:callback];
@@ -29,7 +29,7 @@
                                       });
 	dispatch_source_set_cancel_handler(source, ^(void)
                                        {
-                                           close(fildes);
+                                           close(fileDescriptor);
                                        });
 	dispatch_resume(source);
 }
