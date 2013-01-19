20271249dd2328d46c1a6e947273c18d33c90a3a
Tom Benner <tombenner@gmail.com>
2013-01-18 20:36:33 -0800

    Replaced tabs with spaces in NUIFileMonitor

--- NUI/Core/NUIFileMonitor.m
+++ NUI/Core/NUIFileMonitor.m
@@ -13,12 +13,12 @@
 +(void)watch:(NSString*)path withCallback:(void(^)())callback
 {
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-	int fileDescriptor = open([path UTF8String], O_EVTONLY);
+    int fileDescriptor = open([path UTF8String], O_EVTONLY);
     
     __block dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,fileDescriptor,
                                                               DISPATCH_VNODE_DELETE | DISPATCH_VNODE_WRITE | DISPATCH_VNODE_EXTEND,
                                                               queue);
-	dispatch_source_set_event_handler(source, ^
+    dispatch_source_set_event_handler(source, ^
                                       {
                                           unsigned long flags = dispatch_source_get_data(source);
                                           if (flags & DISPATCH_VNODE_DELETE) {
@@ -27,11 +27,11 @@
                                               [self watch:path withCallback:callback];
                                           }
                                       });
-	dispatch_source_set_cancel_handler(source, ^(void)
+    dispatch_source_set_cancel_handler(source, ^(void)
                                        {
                                            close(fileDescriptor);
                                        });
-	dispatch_resume(source);
+    dispatch_resume(source);
 }
 
 @end
