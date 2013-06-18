3c2f1eb9d0896cbb24b1ab86cae016473a76076c
Micha Mazaheri <micha@mazaheri.me>
2013-06-19 03:46:47 +0900

    Added Xcode project generating libNUI.a so it can be easily imported in another Xcode project. Added Xcode related files to .gitignore. Created NUI.h to import in one place all files a user may need when using NUI.

---
+++ NUI/NUI.h
@@ -0,0 +1,30 @@
+//
+//  NUI.h
+//  NUI
+//
+//  Created by Micha Mazaheri on 6/19/13.
+//  Copyright (c) 2013 NUI. All rights reserved.
+//
+
+#ifdef __OBJC__
+	#import "NUIAppearance.h"
+	#import "NUISettings.h"
+	#import "UIBarButtonItem+NUI.h"
+	#import "UIButton+NUI.h"
+	#import "UIControl+NUI.h"
+	#import "UILabel+NUI.h"
+	#import "UINavigationBar+NUI.h"
+	#import "UINavigationItem+NUI.h"
+	#import "UIProgressView+NUI.h"
+	#import "UISearchBar+NUI.h"
+	#import "UISegmentedControl+NUI.h"
+	#import "UISlider+NUI.h"
+	#import "UISwitch+NUI.h"
+	#import "UITabBar+NUI.h"
+	#import "UITabBarItem+NUI.h"
+	#import "UITableViewCell+NUI.h"
+	#import "UITextField+NUI.h"
+	#import "UIToolbar+NUI.h"
+	#import "UIView+NUI.h"
+	#import "UIWindow+NUI.h"
+#endif
