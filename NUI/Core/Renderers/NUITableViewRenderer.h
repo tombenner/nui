1e92949545b3ec87ee4c8d109bcc35651535fa0e
Tobias Heinicke <tobias.heinicke@david-software.de>
2013-06-20 13:42:39 +0200

    Added support for UITableView. (added missing files)

---
+++ NUI/Core/Renderers/NUITableViewRenderer.h
@@ -0,0 +1,20 @@
+//
+//  NUITableViewRenderer.h
+//  Pods
+//
+//  Created by Tobias Heinicke on 19.06.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+#import "NUIGraphics.h"
+#import "NUIRenderer.h"
+#import "NUISettings.h"
+
+@interface NUITableViewRenderer : NSObject
+
++ (void)render:(UITableView*)tableView withClass:(NSString*)className;
++ (void)sizeDidChange:(UITableView*)tableView;
+
+@end
