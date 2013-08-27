4b246250ab984a4c59f162c08109cccfc1b21a50
Shawn Dempsey <sndempsey@gmail.com>
2013-08-26 17:37:21 -0700

    switch header/main code in text view support.

--- NUI/UI/UITextView+NUI.m
+++ NUI/UI/UITextView+NUI.m
@@ -1,7 +1,34 @@
-#import <UIKit/UIKit.h>
-#import <objc/runtime.h>
-#import "NUIRenderer.h"
+#import "UITextView+NUI.h"
 
-@interface UITextView (NUI)
+@implementation UITextView (NUI)
+
+- (void)initNUI
+{
+    if (!self.nuiClass) {
+        self.nuiClass = @"TextView";
+    }
+}
+
+- (void)applyNUI
+{
+    // Styling shouldn't be applied to inherited classes or to labels within other views
+    // (e.g. UITableViewCellContentView), unless nuiClass is explictly set
+    if (([self class] == [UILabel class] &&
+         [[self superview] class] == [UIView class]) || self.nuiClass) {
+        [self initNUI];
+        if (![self.nuiClass isEqualToString:@"none"]) {
+            [NUIRenderer renderTextView:self withClass:self.nuiClass];
+        }
+    }
+    self.nuiIsApplied = [NSNumber numberWithBool:YES];
+}
+
+- (void)override_didMoveToWindow
+{
+    if (!self.nuiIsApplied) {
+        [self applyNUI];
+    }
+    [self override_didMoveToWindow];
+}
 
 @end
