8863c7b57bf94d18e345c46287929f3ba815c3fd
Matthew Flint <m@tthew.org>
2013-09-15 01:59:52 +0100

    remove orientation-change observer from UITableViewHeaderFooterView

--- NUI/UI/UITableViewHeaderFooterView+NUI.m
+++ NUI/UI/UITableViewHeaderFooterView+NUI.m
@@ -37,6 +37,11 @@
     [self override_didMoveToWindow];
 }
 
+- (void)override_dealloc {
+    [NUIRenderer removeOrientationDidChangeObserver:self];
+    [self override_dealloc];
+}
+
 - (void)orientationDidChange:(NSNotification*)notification
 {
     [NUIRenderer performSelector:@selector(sizeDidChangeForTableViewHeaderFooterView:) withObject:self afterDelay:0];
